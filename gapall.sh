f08f338b40a294dfa0beeb7c870601497dcf97ca
Matthew Litwin <mlitwin@kcptech.com>
2014-12-26 13:25:34 -0800

    Remove andriod specific build

--- gapall.sh
+++ gapall.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 make gapbuild LOCALIZATION=en
-make gapbuild LOCALIZATION=en VARIANT=-android
 make gapbuild LOCALIZATION=es
 
 make gapcommit LOCALIZATION=en
-make gapcommit LOCALIZATION=en VARIANT=-android
 make gapcommit LOCALIZATION=es
 
